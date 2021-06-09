01e4317c192fa80348e52b366f603572295f6fd1
Brian Tang <btang@blt-macbook.lan>
2021-06-09 15:59:57 -0500

    Added age and hearing disoreder questions

--- src/components.js
+++ src/components.js
@@ -7,10 +7,44 @@ import './css/components.css'
 Components
 *******************************************************************************/
 
+let updateChoice;
 
 export function Audio({ name, file }) {
-  return (
-    <audio className='audio-single' controls>
-      <source src={`user-studies/${name}/audio/${file}`} type='audio/mpeg'/>
-    </audio>);
+	return (
+		<audio className='audio-single' controls>
+			<source src={`user-studies/${name}/audio/${file}`} type='audio/mpeg'/>
+		</audio>);
 };
+
+export function Choice({ index, choice, setChoice, label }) {
+	console.log(choice);
+	return (
+		<li className="grid">
+			<input type="radio" id={index} />
+			<div className={`check col-1 ${index === choice ? "selected" : ""}`} onClick={() => setChoice(index)}>
+				<div className="inside"></div>
+			</div>
+			<label className="col-3" onClick={() => setChoice(index)}>{label}</label>
+		</li>
+	);
+};
+
+export function MultipleChoice({ choice, setChoice, labels }) {
+	updateChoice = setChoice;
+	const multipleChoice = labels.map((item, key) =>
+		<Choice 
+			key={key}
+			index={key}
+			choice={choice}
+			setChoice={setChoice}
+			label={item}
+		/>
+	);
+	return (
+		<ul className="MultipleChoice">
+			{multipleChoice}
+		</ul>
+	);
+};
+
+export {updateChoice};
--- src/user-studies/qualification.js
+++ src/user-studies/qualification.js
@@ -1,16 +1,115 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 
 import { Audio } from '../components'
+import { MultipleChoice } from '../components'
+import { updateChoice } from '../components'
 
 import '../css/user-studies.css'
 
+function clickHandler(index, setIndex) {
+	setIndex(index + 1);
+	if (index > 0) {
+		updateChoice(-1);
+	}
+}
+
+function Welcome_Page(index, setIndex) {
+	return (
+		<div className="container grid">
+			<div className="section col-all">
+				<ReactMarkdown source={`# **Welcome!**\nPlease read the following information 
+					carefully before you decide to take part. This will tell you why the 
+					research is being done and what you will be asked to do if you take part. 
+					Please ask if there is anything that is not clear or if you would like 
+					more information.`}
+				/>
+			</div>
+			<div className="section col-2 align-right">
+				<a href="#" className="button" onClick={() => clickHandler(index, setIndex)}>Start</a>
+			</div>
+		</div>
+	);
+	
+}
+
+function Age_Question(index, setIndex, choice, setChoice) {
+	let next_button = null;
+	if (choice !== -1) {
+		next_button = (
+			<div className="section col-2 align-right">
+				<a href="#" className="button" onClick={() => clickHandler(index, setIndex)}>Next</a>
+			</div>
+		);
+	}
+	return (
+		<div className="container grid">
+			<div className="section col-all">
+				<ReactMarkdown source={`**Question ${index}.** Are you at least 18 years old?`}/>
+				<MultipleChoice 
+					index={index} 
+					choice={choice} 
+					setChoice={setChoice}
+					labels={["Yes", "No"]}
+				/>
+			</div>
+			{next_button}
+		</div>
+	);
+}
+
+function Disorder_Question(index, setIndex, choice, setChoice) {
+	let next_button = null;
+	if (choice !== -1) {
+		next_button = (
+			<div className="section col-2 align-right">
+				<a href="#" className="button" onClick={() => clickHandler(index, setIndex)}>Next</a>
+			</div>
+		);
+	}
+	return (
+		<div className="container grid">
+			<div className="section col-all">
+				<ReactMarkdown source={`**Question ${index}.** Have you ever been diagnosed 
+				with hearing loss or a hearing disorder?`}/>
+				<MultipleChoice 
+					index={index}
+					choice={choice}
+					setChoice={setChoice}
+					labels={["Yes", "No"]}
+				/>
+			</div>
+			{next_button}
+		</div>
+	);
+}
+
+function Question_Pages(index, setIndex) {
+	const [choice, setChoice] = useState(-1);
+
+	switch (index) {
+		case 0:
+			return Welcome_Page(index, setIndex);
+		case 1:
+			return Age_Question(index, setIndex, choice, setChoice);
+		case 2:
+			return Disorder_Question(index, setIndex, choice, setChoice);
+		default:
+			return (
+				<div className="container">
+					<ReactMarkdown source={`Thank you! We will contact you shortly`}/>
+				</div>
+			);
+	}
+}
 
 export default function Qualification() {
-  return (
-    <div className='section'>
-      <ReactMarkdown source={'Please listen to the following audio.'}/>
-      <Audio name={'test-abx'} file={'sitting.mp3'}/>
-    </div>
-  );
+
+	const [index, setIndex] = useState(0);
+
+	return (
+		<div>
+			{Question_Pages(index, setIndex)}
+		</div>
+	);
 }
